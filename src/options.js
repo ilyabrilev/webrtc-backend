818c0435ae05cb503d62bf24bffddd37924d1c85
ilya <i.brylev@2-up.ru>
2023-08-01 12:27:38 +0300

    deploy

--- src/options.js
+++ src/options.js
@@ -1,17 +1,22 @@
 const fs = require('fs');
 
-const keyFilename = process.env.KEY_PATH;
-const certFilename = process.env.CERT_PATH;
+let expressOptions = {};
 
-if (!fs.existsSync(keyFilename) || !fs.existsSync(certFilename)) {
+if (process.env.HTTPS_DEV) {
+
+  const keyFilename = process.env.KEY_PATH;
+  const certFilename = process.env.CERT_PATH;
+
+  if (!fs.existsSync(keyFilename) || !fs.existsSync(certFilename)) {
     throw new Error('Cert files not found');
-}
+  }
 
-const key = fs.readFileSync(keyFilename);
-const cert = fs.readFileSync(certFilename);
-const expressOptions = {
-  key,
-  cert,
-};
+  const key = fs.readFileSync(keyFilename);
+  const cert = fs.readFileSync(certFilename);
+  expressOptions = {
+    key,
+    cert,
+  };
+}
 
 module.exports = expressOptions;
