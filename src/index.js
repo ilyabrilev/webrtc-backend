ca487f6bb28a6d4aa781edbecd6317f7ce0bca00
ilya <i.brylev@2-up.ru>
2023-08-01 15:20:09 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -7,27 +7,30 @@ const setupPeer = require( './peer.js');
 const router = require('./router.js');
 const expressOptions = require('./options.js');
 
-
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(router);
 
-
-let expressServer;
-if (process.env.HTTPS_DEV) {
-    // start
-    expressServer = https.createServer(expressOptions, app);
-    expressServer.listen(process.env.APP_PORT, () => {
-        console.log(`listening on ${process.env.APP_URL}${process.env.APP_PORT ? ':'+process.env.APP_PORT : ''}`);
-    });
-} else {
-    expressServer = http.createServer(expressOptions, app);
-    expressServer.listen(process.env.APP_PORT, () => {
-        console.log(`listening on ${process.env.APP_URL}${process.env.APP_PORT ? ':'+process.env.APP_PORT : ''}`);
-    });    
-}
+expressServer.listen(process.env.APP_PORT, () => {
+    console.log(`listening on ${process.env.APP_URL}${process.env.APP_PORT ? ':'+process.env.APP_PORT : ''}`);
+});
 
 
-setupPeer(expressOptions);
-setupSocketServer(expressServer);
+// let expressServer;
+// if (process.env.HTTPS_DEV) {
+//     // start
+//     expressServer = https.createServer(expressOptions, app);
+//     expressServer.listen(process.env.APP_PORT, () => {
+//         console.log(`listening on ${process.env.APP_URL}${process.env.APP_PORT ? ':'+process.env.APP_PORT : ''}`);
+//     });
+// } else {
+//     expressServer = http.createServer(expressOptions, app);
+//     expressServer.listen(process.env.APP_PORT, () => {
+//         console.log(`listening on ${process.env.APP_URL}${process.env.APP_PORT ? ':'+process.env.APP_PORT : ''}`);
+//     });    
+// }
+
+
+// setupPeer(expressOptions);
+// setupSocketServer(expressServer);
